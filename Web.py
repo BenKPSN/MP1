1290843b085251541859c4f9a5eb7db767425d7d
BenN <benshiresnakamura35@gmail.com>
2024-06-18 14:23:50 -0700

    update web.py

--- Web.py
+++ Web.py
@@ -4,36 +4,39 @@ from threading import *
 host = ''
 port = 8888
 
-s = socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket(AF_INET, SOCK_STREAM)
 s.bind((host, port))
-s.listen(1)
+s.listen(5)
+print(s)
+
+
+def client_thread(conn):
+    while True:
+      data = conn.recv(4096)
+
+      print(data)
+      # CHECK STATUS CODES
+
+
+
+
+      test = open('test.html','rb')
+      file = test.read()
+      test.close()
+
+      head = b"HTTP/1.1 200 OK\n"
+      response = head + file
+      conn.send(response)
+
+
+    conn.close()
+        
+
 
 while True:
     clientsocket, address = s.accept()
 
-    #For now, this is discarded. When we check if the connection is correct we need to look at this.
-    reveived_data = clientsocket.recv(1024).decode()
-    
-    http_response = """
-HTTP/1.1 200 OK
-<html>
+    client_thread(clientsocket)
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="author" content="">
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</head>
 
-<body>
-
-  <p>Congratulations! Your Web Server is Working!</p>
-
-</body>
-
-</html>
-"""
-    clientsocket.send(http_response.encode())
-    clientsocket.close()
